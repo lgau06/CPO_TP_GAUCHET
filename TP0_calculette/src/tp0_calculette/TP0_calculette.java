818f365011c64bb4cc5ac0e7a81f90430aead4ba
lisagauchet <lisagauchet@localhost>
2025-10-24 11:43:40 +0200

    Ajout TPO calculette

---
+++ TP0_calculette/src/tp0_calculette/TP0_calculette.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_calculette;
+
+/**
+ *
+ * @author lisagauchet
+ */
+public class TP0_calculette {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Please enter the operator:");
+        System.out.println("1) add");
+        System.out.println("2) substract");
+        System.out.println("3) multiply");
+        System.out.println("4) divide");
+        System.out.println("5) modulo");
+    }
+    
+}
