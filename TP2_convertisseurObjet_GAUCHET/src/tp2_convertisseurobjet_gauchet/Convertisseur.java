219cfa0aedec016c2a3f2297bd6c3b2d19081f9f
lisagauchet <lisagauchet@localhost>
2025-11-18 11:51:55 +0100

    Ajout TP2 Convertisseur

--- TP2_convertisseurObjet_GAUCHET/src/tp2_convertisseurobjet_gauchet/Convertisseur.java
+++ TP2_convertisseurObjet_GAUCHET/src/tp2_convertisseurobjet_gauchet/Convertisseur.java
@@ -9,47 +9,41 @@ package tp2_convertisseurobjet_gauchet;
  * @author lisagauchet
  */
 public class Convertisseur {
-    int nbConversions;
+    int nbConversions = 0;
 
-    
-    public Convertisseur() {
-        nbConversions = 0;
-    }
-
-    
-    public float CelciusVersKelvin(float tCelcius) {
+    public float CelciusVersKelvin(float t) {
         nbConversions++;
-        return tCelcius + 273.15f;
+        return t + 273.15f;
     }
 
-    public float KelvinVersCelcius(float tKelvin) {
+    public float KelvinVersCelcius(float t) {
         nbConversions++;
-        return tKelvin - 273.15f;
+        return t - 273.15f;
     }
 
-    public float FarenheitVersCelcius(float tFarenheit) {
+    public float FarenheitVersCelcius(float t) {
         nbConversions++;
-        return (tFarenheit - 32) * 5 / 9;
+        return (t - 32) * 5 / 9;
     }
 
-    public float CelciusVersFarenheit(float tCelcius) {
+    public float CelciusVersFarenheit(float t) {
         nbConversions++;
-        return (tCelcius * 9 / 5) + 32;
-    }
-    public float FarenheitVersKelvin(float tFarenheit) {
-        nbConversions++;
-        return (tFarenheit - 32) * 5 / 9 + 273.15f;
+        return (t * 9 / 5) + 32;
     }
 
-    public float KelvinVersFarenheit(float tKelvin) {
+    public float FarenheitVersKelvin(float t) {
         nbConversions++;
-        return (tKelvin - 273.15f) * 9 / 5 + 32;
+        return (t - 32) * 5 / 9 + 273.15f;
     }
 
-    // Méthode toString
+    public float KelvinVersFarenheit(float t) {
+        nbConversions++;
+        return (t - 273.15f) * 9 / 5 + 32;
+    }
+
     @Override
     public String toString() {
-        return "Nombre de conversions effectuées : " + nbConversions;
+        return "Nombre de conversions : " + nbConversions;
     }
 }
 
