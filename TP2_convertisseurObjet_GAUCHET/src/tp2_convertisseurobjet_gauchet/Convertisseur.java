634afe83ffdadeb1cbd3f0b36637d91796e65646
lisagauchet <lisagauchet@localhost>
2025-11-18 11:45:00 +0100

    Ajout TP2 Convertisseur

--- TP2_convertisseurObjet_GAUCHET/src/tp2_convertisseurobjet_gauchet/Convertisseur.java
+++ TP2_convertisseurObjet_GAUCHET/src/tp2_convertisseurobjet_gauchet/Convertisseur.java
@@ -9,5 +9,48 @@ package tp2_convertisseurobjet_gauchet;
  * @author lisagauchet
  */
 public class Convertisseur {
+    int nbConversions;
+
     
+    public Convertisseur() {
+        nbConversions = 0;
+    }
+
+    
+    public float CelciusVersKelvin(float tCelcius) {
+        nbConversions++;
+        return tCelcius + 273.15f;
+    }
+
+    public float KelvinVersCelcius(float tKelvin) {
+        nbConversions++;
+        return tKelvin - 273.15f;
+    }
+
+    public float FarenheitVersCelcius(float tFarenheit) {
+        nbConversions++;
+        return (tFarenheit - 32) * 5 / 9;
+    }
+
+    public float CelciusVersFarenheit(float tCelcius) {
+        nbConversions++;
+        return (tCelcius * 9 / 5) + 32;
+    }
+    public float FarenheitVersKelvin(float tFarenheit) {
+        nbConversions++;
+        return (tFarenheit - 32) * 5 / 9 + 273.15f;
+    }
+
+    public float KelvinVersFarenheit(float tKelvin) {
+        nbConversions++;
+        return (tKelvin - 273.15f) * 9 / 5 + 32;
+    }
+
+    // Méthode toString
+    @Override
+    public String toString() {
+        return "Nombre de conversions effectuées : " + nbConversions;
+    }
 }
+
+
--- TP2_convertisseurObjet_GAUCHET/src/tp2_convertisseurobjet_gauchet/TP2_convertisseurObjet_GAUCHET.java
+++ TP2_convertisseurObjet_GAUCHET/src/tp2_convertisseurobjet_gauchet/TP2_convertisseurObjet_GAUCHET.java
@@ -4,6 +4,8 @@
  */
 package tp2_convertisseurobjet_gauchet;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lisagauchet
@@ -14,7 +16,59 @@ public class TP2_convertisseurObjet_GAUCHET {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        Convertisseur conv = new Convertisseur();
+
+        System.out.println("=== Convertisseur de Températures ===");
+        System.out.println("1 - Celsius vers Kelvin");
+        System.out.println("2 - Kelvin vers Celsius");
+        System.out.println("3 - Fahrenheit vers Celsius");
+        System.out.println("4 - Celsius vers Fahrenheit");
+        System.out.println("5 - Fahrenheit vers Kelvin");
+        System.out.println("6 - Kelvin vers Fahrenheit");
+
+        System.out.print("Choisissez une conversion : ");
+        int choix = sc.nextInt();
+
+        System.out.print("Entrez la température à convertir : ");
+        float temp = sc.nextFloat();
+
+        float resultat = 0;
+
+        switch (choix) {
+            case 1:
+                resultat = conv.CelciusVersKelvin(temp);
+                System.out.println(temp + " °C = " + resultat + " K");
+                break;
+            case 2:
+                resultat = conv.KelvinVersCelcius(temp);
+                System.out.println(temp + " K = " + resultat + " °C");
+                break;
+            case 3:
+                resultat = conv.FarenheitVersCelcius(temp);
+                System.out.println(temp + " °F = " + resultat + " °C");
+                break;
+            case 4:
+                resultat = conv.CelciusVersFarenheit(temp);
+                System.out.println(temp + " °C = " + resultat + " °F");
+                break;
+            case 5:
+                resultat = conv.FarenheitVersKelvin(temp);
+                System.out.println(temp + " °F = " + resultat + " K");
+                break;
+            case 6:
+                resultat = conv.KelvinVersFarenheit(temp);
+                System.out.println(temp + " K = " + resultat + " °F");
+                break;
+            default:
+                System.out.println("Choix invalide !");
+                break;
+        }
+
+        System.out.println(conv.toString());
+        sc.close();
     }
-    
 }
+    
+    
+
